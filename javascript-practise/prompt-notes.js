2b6bc848f038f79e127c815587acf9f54b3e90f3
SafnetCo2 <nzi19001@byui.edu>
2023-06-06 21:37:24 +0300

    javascript practise

---
+++ javascript-practise/prompt-notes.js
@@ -0,0 +1,18 @@
+//use strict mode
+//Eliminates some javascript silent errors by changing them to throw errors.
+//Fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes be made to run faster than identical code that's not strict mode.
+"use strict"
+//require loads this funtion which prompt-sync
+//exports a this function using a variable ps
+const prompt = require("prompt-sync")({sigint: true});
+
+//const ps = require("prompt-sync");
+//const prompt = ps({sigint: true});/
+
+// function that creates prompt callthe function by referencing ps
+//pass config informationJo on ps function to terminate the  (sigint) that is if any interrupt signal is generated treat it accordingly
+let name = prompt("Enter your name: ")//user input
+console.log(`Hello ${name}`)
+//use prompt to 
+//taken input from the user
+//display output
