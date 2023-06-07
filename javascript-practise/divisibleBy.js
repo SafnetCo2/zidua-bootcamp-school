59965c9ef44dcca44fe21787a5b48fc1a6bd6202
SafnetCo2 <nzi19001@byui.edu>
2023-06-07 10:48:23 +0300

    update

---
+++ javascript-practise/divisibleBy.js
@@ -0,0 +1,7 @@
+let num = 9;
+if(num % 3 ==0){
+    console.log(`The number is divisible by 3 ${num}`);
+}
+else{
+    console.log(`Not divisible by 3 ${num}`)
+}
---
+++ javascript-practise/loop.js
@@ -0,0 +1,8 @@
+
+//print numbers between 1 and 100
+for(let i =1; i<= 100;  i++){ 
+    //check if this number is even
+    if (i % 2=== 0) {
+      console.log(i)
+      }
+}
