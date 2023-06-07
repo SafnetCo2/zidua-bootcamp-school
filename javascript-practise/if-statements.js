fcaf96a75b2d6f57dfa85b4f2b6e15898de30684
SafnetCo2 <nzi19001@byui.edu>
2023-06-07 13:22:45 +0300

    if  statements

---
+++ javascript-practise/if-statements.js
@@ -0,0 +1,12 @@
+var count =3;
+if(count ==4){
+    console.log("count is 4")
+}else if(count >4){
+    console.log('greater than 4')
+
+}
+else if(count < 4){
+    console.log('less than 4')
+}else{
+    console.log('not equal to 4')
+}
