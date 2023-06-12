a366216a354d707f5d6a76f8c4ca6e9989078d12
SafnetCo2 <nzi19001@byui.edu>
2023-06-12 22:08:31 +0300

    styles

--- javascript-practise/12-6-2023/main.js
+++ javascript-practise/12-6-2023/main.js
@@ -1,12 +1,14 @@
 const para1 = document.getElementById('para');
 const cardBtn = document.getElementById('btn');
-
 const imageCard =document.getElementById('img');
 
+
+
 cardBtn.addEventListener('click', function(){
-document.getElementById('p').innerHTML='Hello World! this my new car';
-//imageCard.style.backgroundImage = 'url("assets/serjan-midili-sMYh2NynuK0-unsplash.jpg")';
-// document.getElementById('img').innerHTML='url("assets/serjan-midili-sMYh2NynuK0-unsplash.jpg")';
+    para1.style.backgroundColor='green';
+    
+document.getElementById('para').innerHTML='Hello World! this my new car red in color';
+
 
 });
 
