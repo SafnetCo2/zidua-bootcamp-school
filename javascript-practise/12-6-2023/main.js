8803d8300b4bd040577b47f0059ccea4654b5b2f
SafnetCo2 <nzi19001@byui.edu>
2023-06-12 22:19:34 +0300

    color styles

--- javascript-practise/12-6-2023/main.js
+++ javascript-practise/12-6-2023/main.js
@@ -1,16 +1,15 @@
 const para1 = document.getElementById('para');
 const cardBtn = document.getElementById('btn');
+const reset= document.getElementById('clear')
 const imageCard =document.getElementById('img');
 
 
 
 cardBtn.addEventListener('click', function(){
-    para1.style.backgroundColor='green';
+    para1.style.color='white';
+    
     
 document.getElementById('para').innerHTML='Hello World! this my new car red in color';
 
 
 });
-
-
-    
