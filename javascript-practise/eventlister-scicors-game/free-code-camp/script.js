1793995098bcf1b79ce8e7f09d131aecd1d53274
SafnetCo2 <nzi19001@byui.edu>
2023-06-14 21:39:22 +0300

    create new folders

---
+++ javascript-practise/eventlister-scicors-game/free-code-camp/script.js
@@ -0,0 +1,79 @@
+let userScore = 0;
+let compScore = 0;
+//select elements
+const userScore_span =document.getElementById('user-score');
+const compScore_span =document.getElementById('comp-score');
+
+const scoreBoard_div =document.querySelector('.score-board');
+const result =document.querySelector('result >p');
+
+const faCheck = document.getElementById('r');
+const faxmark =document.getElementById('p');
+const farrowright =document.getElementById('s');
+//add eventlistener 
+
+function getComputerChoice(){
+    const choices = ['r','p','r'];
+    const randomNumber=Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+
+
+function win(userChoice,computerChoice){
+userScore++;
+userScore_span.innerHTML=userScore;
+compScore_span.innerHTML=compScore;
+result.innerHTML = userChoice + 'beats' + computerChoice + 'You Win';
+}
+
+function lose(){
+    
+}
+function draw(){
+    
+}
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice){
+        case 'rs':
+        case 'pr':
+        case 'sp':
+        ('perfect you win');
+        break;
+
+        case 'rp':
+        case 'ps':
+        case 'sr':
+        console.log('oops loose');
+        break;
+
+        case 'rr':
+        case 'pp':
+        case 'ss':
+        console.log('draw');
+        break;
+    }
+
+
+
+}
+
+// function main which nest userall
+
+function play(){
+    faCheck.addEventListener('click',function(){
+      game('r');
+    });
+    
+    faxmark.addEventListener('click',function(){
+        game('p');
+    });
+    farrowright.addEventListener('click',function(){
+        game('s');
+    });
+}
+
+play();
+
+//create function game
---
+++ javascript-practise/eventlister-scicors-game/kens-game/ken.js
@@ -0,0 +1,64 @@
+var you;
+var yourScore =0;
+var opponent;
+var opponentScore=0;
+var choices =['rock','paper','scissors'];
+window.onload =function(){
+    for(let i =0; i<3; i++){
+        let choice =document.createElement('img');
+        choice.id =choices[1];
+        choices.src =choices[i] + '.jpg';
+        choice.addEventListener('click', selectChoice);
+        document.getElementById('choices').append(choice);
+    }
+};
+
+function selectChoice(){
+    you = this.id;
+    document.getElementById('your-choice').src=you + '.jng';
+    
+    //random for opponet
+    opponent = choices[Math.floor(Math.random()*3)];
+    document.getElementById('opponent-choice').src =opponent + '.jng';
+    //check the winneryou 
+    if(you == opponent){
+        yourScore +=1;
+        opponentScore +=1;
+    }else{
+        if(you =='rock'){
+            if(opponent == 'scissors'){
+                yourScore += 1;
+
+            }
+            else if(opponent == 'paper'){
+                opponentScore +=1;
+
+            }
+        
+        
+        }
+        else if(you =='scissors'){
+           if(opponent == 'paper'){
+            yourScore += 1;
+
+          }
+        else if(opponent == 'rock')
+          opponentScore+=1;
+        }
+
+        else if(you =='paper'){
+        if(opponent == 'rock'){
+            yourScore += 1;
+
+        }
+        else if(opponent == 'scissors')
+        opponentScore+=1;
+    }
+    document.getElementById('your-score').innerText=yourScore;
+    document.getElementById('opponet-score').innerText=opponetScore;
+ }
+
+}
+
+
+
