d75df955f2d9e77eeb6b6573d38c42d3d62bc6e2
SafnetCo2 <nzi19001@byui.edu>
2023-06-25 20:19:00 +0300

    update code

--- Zidua-challenges-updated/array-manipulation-challenge/script.js
+++ Zidua-challenges-updated/array-manipulation-challenge/script.js
@@ -7,12 +7,12 @@ Use filter to keep only values that are greater than 10.
 Use reduce to find the sum of the remaining values
 The function should return the final sum. */
 
-// const arr = [1, 2, 3, 4, 5, 6, 7];
+ const arr = [1, 2, 3, 4, 5, 6, 7];
 
 // const newArray = arr.filter(a => a % 2 === 0);
 // console.log(newArray);
-//  arr= [1, 2, 4, 8];
-//const numbers = [1, 2, 4, 8];
+// const  arr= [1, 2, 4, 8];
+// const numbers = [1, 2, 4, 8];
 // const sum = numbers.reduce((num1, num2) => {
 //     //console.log(num1, num2);
 //     return num1 + num2;
@@ -20,9 +20,9 @@ The function should return the final sum. */
 // console.log(sum);
 function manipulateArray(arr) {
 
-    return arr*3;
+    return arr;
 }
-arr = new Array(1, 2, 4, 8);
+// arr = new Array(1, 2, 4, 8);
     const new_Array = arr
         .filter(a => a % 2 === 0)
         .map(a => a * a)//square of a number
